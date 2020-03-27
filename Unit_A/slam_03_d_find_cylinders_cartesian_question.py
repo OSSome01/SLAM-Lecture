c01a1b49e95766729ae431aaa7ec7b406f0dbcd3
OSSome01 <omkarsargar01@gmail.com>
2020-03-27 15:51:17 +0530

    changes in func find_cylinder

--- Unit_A/slam_03_d_find_cylinders_cartesian_question.py
+++ Unit_A/slam_03_d_find_cylinders_cartesian_question.py
@@ -24,51 +24,54 @@ def compute_derivative(scan, min_dist):
 # For each area between a left falling edge and a right rising edge,
 # determine the average ray number and the average depth.
 def find_cylinders(scan, scan_derivative, jump, min_dist):
-    # print(scan_derivative)
     cylinder_list = []
     on_cylinder = False
     sum_ray, sum_depth, rays = 0.0, 0.0, 0
-    # print(scan_derivative)
-    count = 0
-    for i in xrange(len(scan_derivative)-1):
+    discard = False
+    direction = 'Left'
+
+    for i in range(len(scan_derivative)):
         # --->>> Insert your cylinder code here.
         # Whenever you find a cylinder, add a tuple
         # (average_ray, average_depth) to the cylinder_list.
-        if on_cylinder == False and scan_derivative[i] <= -100:
-            on_cylinder = True
-            # print "negative edge detected at i = ", i
-            # print "scan_derevative[i] =", scan_derivative[i]
-        elif on_cylinder == True:
-            # print "rays = ", rays
-            # print "sum_rays = ", sum_ray
-            if scan_derivative[i] <= -100:
-                # print "second negative edge detected at i = ",i
-                # print "scan_derevative[i] =", scan_derivative[i]
-                rays = 0
-                sum_ray = 0
-                sum_depth = 0 
-            rays = rays + 1
-            sum_ray = sum_ray + i
-            sum_depth = sum_depth + scan[i]
-            if scan_derivative[i] >= 100:
-                # print "positive edge detected at i = ", i
-                # print "scan_derevative[i] =", scan_derivative[i]
-                average_ray = int((sum_ray)/rays)
-                on_cylinder = False
-                # print "cylinder is at i = ", average_ray
-                cylinder_list.append((average_ray, scan[average_ray]))
-                count = count + 1
-                # print "No. of cylinders detected = ",count
-                rays = 0
-                sum_ray = 0
-                sum_depth = 0 
-        # Just for fun, I'll output some cylinders.
-        # Replace this by your code.
-        # if i % 100 == 0:
-        #     cylinder_list.append( (i, scan[i]) )
+        current_der = scan_derivative[i]
+
+        if abs(current_der) > jump:
+            if on_cylinder and direction == 'Left':
+                if current_der < 0:  # Left again
+                    discard = True
+                else:
+                    on_cylinder = False
+                    average_ray = sum_ray / rays
+                    average_depth = sum_depth / rays
+                    cylinder_list.append((average_ray, average_depth))
+                    sum_ray, sum_depth, rays = 0.0, 0.0, 0
+
+            if not on_cylinder and current_der < 0:
+                on_cylinder = True
+                # if current_der > 0:
+                #     direction = 'Right'
+                # elif current_der < 0:
+                #     direction = 'Left'
+                direction = 'Left'
+
+        if scan[i] <= min_dist and not on_cylinder:
+            discard = True
+
+        if scan[i] <= min_dist and on_cylinder:
+            continue
+
+        if on_cylinder and scan[i] > min_dist:
+            rays += 1
+            sum_ray += i
+            sum_depth += scan[i]
+
+        if discard:
+            sum_ray, sum_depth, rays = 0.0, 0.0, 0
+            discard = False
 
     return cylinder_list
-
+    
 def compute_cartesian_coordinates(cylinders, cylinder_offset):
     result = []
     for c in cylinders:
